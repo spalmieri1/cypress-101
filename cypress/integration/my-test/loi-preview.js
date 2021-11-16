fda7fa5095504e90fbd872da16379e121f59cbe5
spalmieri1 <stephen.palmieri@viewthespace.com>
2021-11-16 11:24:52 -0500

    Removing test text

--- cypress/integration/my-test/loi-preview.js
+++ cypress/integration/my-test/loi-preview.js
@@ -94,7 +94,6 @@ describe('LOI Preview Tests', () => {
         cy.contains('THIRTY-FIVE DOLLARS ($35.00) PER RENTABLE SQUARE FOOT PER MONTH')
         cy.contains('thirty-five dollars ($35.00) per rentable square foot per month')
         cy.contains('$35.00 /sf/mo')  
-        Hello
-        
+
     })
 })
