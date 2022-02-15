3bd793cbf00ce8cce85ad51c14a0e3d0412ea45e
spalmieri1 <stephen.palmieri@viewthespace.com>
2022-02-15 15:56:28 -0500

    removed commented out items

--- cypress/integration/my-test/vts-login-test.js
+++ cypress/integration/my-test/vts-login-test.js
@@ -1,16 +1,4 @@
 /// <reference types="cypress" />
-
-//describe 
-
-// -> context 
-//    -> it
-
-
-// before
-// beforeEach
-// after
-// afterEach
-
 describe('VTS login test', () => {
 
     before(() => {
